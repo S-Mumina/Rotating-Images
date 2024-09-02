01313afe0b7a962b21eb1e379ca45ce39b8c6171
S-Mumina <muasyamumina@gmail.com>
2024-09-02 08:58:39 +0300

    setTimeout

--- index.js
+++ index.js
@@ -3,15 +3,25 @@ const prevEl = document.getElementById("prev")
 const nextEl = document.getElementById("next")
 
 let x = 0
+timer;
 prevEl.addEventListener("click",()=>{
  x = x + 45;
+ clearTimeout(timer)
  updateGallery()
 });
 nextEl.addEventListener("click",()=>{
  x = x - 45;
+ clearTimeout(timer)
  updateGallery()
-});
+});c
 
 function updateGallery(){
     imageContainerEl.style.transform = `pespective(100px) rotateY(${x}deg)`
+    timer = setTimeout(()=>{
+      x = x - 45 
+      updateGallery
+
+    },3000)
 };
+
+updateGallery()
